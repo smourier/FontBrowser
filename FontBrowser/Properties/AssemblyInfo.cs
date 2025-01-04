70cbf3ee311ae84b083e24410d28452a0f945313
Simon Mourier <smourier@users.noreply.github.com>
2025-01-04 12:23:58 +0100

    updated copyrights

--- FontBrowser/Properties/AssemblyInfo.cs
+++ FontBrowser/Properties/AssemblyInfo.cs
@@ -5,7 +5,7 @@ using System.Windows;
 [assembly: AssemblyTitle("FontBrowser")]
 [assembly: AssemblyDescription("A WPF application that shows details of font files. All WPF supported font file types can be displayed.")]
 [assembly: AssemblyProduct("FontBrowser")]
-[assembly: AssemblyCopyright("Copyright © 2016-2018 Simon Mourier. All rights reserved.")]
+[assembly: AssemblyCopyright("Copyright © 2016-2025 Simon Mourier. All rights reserved.")]
 [assembly: ComVisible(false)]
 [assembly: ThemeInfo(ResourceDictionaryLocation.None, ResourceDictionaryLocation.SourceAssembly)]
 [assembly: AssemblyVersion("1.0.0.0")]
